a101447fd36b9ef8f410f0b5a1bc4307b312dac0
Jean Baptiste <shumbushojean1@gmail.com>
2025-08-12 12:17:24 +0200

    after add rule to stop to push on main

--- jose.php
+++ jose.php
@@ -4,4 +4,6 @@ echo "This is the automation testing script for Jose.\n";
 echo "This is another line for Jose's automation testing script.\n";
 echo "Yet another line for Jose's automation testing script.\n";
 echo "Final line for Jose's automation testing script.\n";
+
+echo "This is the last line for Jose's automation testing script.\n after rules";
 ?>
