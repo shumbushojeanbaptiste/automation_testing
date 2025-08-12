51cec18709cced7f63194c1ed0e52429eff6f9ff
Jean Baptiste SHUMBUSHO <136917861+shumbushojeanbaptiste@users.noreply.github.com>
2025-08-12 16:50:46 +0200

    Feature/jose (#14), jose change and cerify deploymeny
    * rule testing
    
    * rule testing v2
    
    * changes in other file
    
    * jose changes and teamate
    
    * jose changes and teamate v2
    
    * Update ci.yml
    
    * jose changes and teamate v3
    
    * updated from Jose ITEC
    
    * after add ITEC mission
    
    * changes from Jose main branch ocally
    
    * after deployment FTP set
    
    * after deployment FTP set v2
    
    * deploy yml changed

--- other.php
+++ other.php
@@ -1,5 +1,13 @@
 <?php 
 echo "Dev jose and teamates";
 echo " and teamates and ITEC international mission is software development";
+<<<<<<< HEAD
 echo "Jean Baptiste";
+=======
+<<<<<<< HEAD
+echo "other teamates";
+=======
+echo "Jean Baptiste";
+>>>>>>> cf9e999865b20e4157bb2a08575fca147c16111e
+>>>>>>> main
 ?>
