2dc3289aa396fc8b59653044946dcde52cd227da
Jean Baptiste <shumbushojean1@gmail.com>
2025-08-12 11:55:44 +0200

    jose wrong push to main

--- jose.php
+++ jose.php
@@ -1,3 +1,7 @@
 <?php   
 echo "This is the automation testing script for Jose.\n"; 
+
+echo "This is another line for Jose's automation testing script.\n";
+echo "Yet another line for Jose's automation testing script.\n";
+echo "Final line for Jose's automation testing script.\n";
 ?>
