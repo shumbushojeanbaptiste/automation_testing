61e0b952bf26aeb368d0870796d844c57819823a
Jean Baptiste <shumbushojean1@gmail.com>
2025-08-12 18:03:34 +0200

    conflict resolve

--- other.php
+++ other.php
@@ -1,29 +1,5 @@
 <?php 
 echo "Dev jose and teamates";
 echo " and teamates and ITEC international mission is software development";
-<<<<<<< HEAD
-<<<<<<< HEAD
-echo "Jean Baptiste";
-=======
-=======
->>>>>>> main
-<<<<<<< HEAD
-echo "other teamates";
-=======
-echo "Jean Baptiste";
->>>>>>> cf9e999865b20e4157bb2a08575fca147c16111e
-<<<<<<< HEAD
->>>>>>> main
-=======
-=======
-echo "Jean Baptiste";
-=======
-<<<<<<< HEAD
-echo "other teamates";
-=======
-echo "Jean Baptiste";
->>>>>>> cf9e999865b20e4157bb2a08575fca147c16111e
->>>>>>> main
->>>>>>> bb6a1406ba4fc7990ec765b1dfda3fbf6c530979
->>>>>>> main
+
 ?>
