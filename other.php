55ea772b21137dabd19e6bfd6288ff3724bcac43
Jean Baptiste SHUMBUSHO <136917861+shumbushojeanbaptiste@users.noreply.github.com>
2025-08-12 18:12:37 +0200

    Feature/jose (#17)
    * rule testing
    
    * rule testing v2
    
    * changes in other file
    
    * jose changes and teamate
    
    * jose changes and teamate v2
    
    * Update ci.yml
    
    * jose changes and teamate v3
    
    * updated from Jose ITEC
    
    * after add ITEC mission
    
    * changes from Jose main branch ocally
    
    * after deployment FTP set
    
    * after deployment FTP set v2
    
    * deploy yml changed
    
    * file change
    
    * enable passeive pass
    
    * some change in deploy yml
    
    * deploy change and CI check code quality
    
    * conflict resolve
    
    * CI file and deloy changed
    
    * CI file and deloy changed

--- other.php
+++ other.php
@@ -2,19 +2,28 @@
 echo "Dev jose and teamates";
 echo " and teamates and ITEC international mission is software development";
 <<<<<<< HEAD
+
+=======
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
 echo "Jean Baptiste";
 =======
 =======
 >>>>>>> main
 <<<<<<< HEAD
+>>>>>>> 817876f4ce67defe9e1cb658c3ac3ad4ea018938
 echo "other teamates";
 =======
 echo "Jean Baptiste";
 >>>>>>> cf9e999865b20e4157bb2a08575fca147c16111e
 <<<<<<< HEAD
+=======
+<<<<<<< HEAD
 >>>>>>> main
 =======
+>>>>>>> 817876f4ce67defe9e1cb658c3ac3ad4ea018938
 =======
 echo "Jean Baptiste";
 =======
@@ -25,5 +34,9 @@ echo "Jean Baptiste";
 >>>>>>> cf9e999865b20e4157bb2a08575fca147c16111e
 >>>>>>> main
 >>>>>>> bb6a1406ba4fc7990ec765b1dfda3fbf6c530979
+<<<<<<< HEAD
+=======
+>>>>>>> main
+>>>>>>> 817876f4ce67defe9e1cb658c3ac3ad4ea018938
 >>>>>>> main
 ?>
