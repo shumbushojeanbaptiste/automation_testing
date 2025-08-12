50f48fdee835174e70c779860f229c039dd80ef0
Jean Baptiste <shumbushojean1@gmail.com>
2025-08-12 12:28:01 +0200

    same branch name

--- jose.php
+++ jose.php
@@ -6,4 +6,5 @@ echo "Yet another line for Jose's automation testing script.\n";
 echo "Final line for Jose's automation testing script.\n";
 
 echo "This is the last line for Jose's automation testing script.\n after rules";
+echo "after rule fail";
 ?>
